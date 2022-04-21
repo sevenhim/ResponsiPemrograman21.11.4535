6b58f510c56baf3d94933ec640fa66b6f96c77e2
sevenhim <101778766+sevenhim@users.noreply.github.com>
2022-04-21 09:34:52 +0700

    Class Karyawan

---
+++ Karyawan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4535
+{
+    public class Karyawan
+    {
+        public string NIK { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public Karyawan()
+        {
+
+        }
+
+        public Karyawan(string nama, string nik, int gajibulanan)
+        {
+            Nama= nama;
+            NIK= nik;
+            GajiBulanan= gajibulanan;
+        }
+    }
+}
